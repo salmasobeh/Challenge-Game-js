de21f5b412852b48320494c7d2ef68c52732e38f
salmasobeh <salma.sobeh97@gmail.com>
2022-01-27 20:21:14 +0200

    Start when user press the space

--- gamejquery.js
+++ gamejquery.js
@@ -5,3 +5,16 @@ var userpattern = []
 var game_start = false
 var btn_colors = ["blue", "green", "red", "yellow"]
 var btn = document.getElementsByClassName("btn")
+
+
+
+/// this part is related to pressing the keyboard to the start
+//-----------------------------------------------------------
+$(document).keypress(function() {
+    game_start = true
+    if (game_start) {
+        $("#title").text("level" + " " + level);
+        patterns();
+        game_start = true;
+    }
+});
