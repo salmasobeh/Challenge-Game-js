d49b0ad709277167e35856ad87f6a69414aa1c17
salmasobeh <salma.sobeh97@gmail.com>
2022-01-27 20:20:32 +0200

    initializng variables in the Jquery file

---
+++ gamejquery.js
@@ -0,0 +1,7 @@
+//defining the variables 
+var level = 0;
+var pcpattern = []
+var userpattern = []
+var game_start = false
+var btn_colors = ["blue", "green", "red", "yellow"]
+var btn = document.getElementsByClassName("btn")
