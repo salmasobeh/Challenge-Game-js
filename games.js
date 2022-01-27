2cf308ab0446ace55ca7873f72be6bf058d8f393
salmasobeh <salma.sobeh97@gmail.com>
2022-01-27 20:12:08 +0200

    Start by pressing on Space

--- games.js
+++ games.js
@@ -5,3 +5,16 @@ var userpattern = []
 var game_start = false
 var btn_colors = ["blue", "green", "red", "yellow"]
 var btn = document.getElementsByClassName("btn")
+
+/// this part is related to pressing the keyboard to the start
+//-----------------------------------------------------------
+document.addEventListener('keyup', event => {
+    game_start = true
+    if (event.code === 'Space') {
+        if (game_start) {
+            document.getElementById("title").innerHTML = "level" + " " + level
+            patterns()
+            game_start = true
+        }
+    }
+})
